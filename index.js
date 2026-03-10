9402e133f71cd3c45f58126ba42a36eb1397c991
Pedro Manoel <pedroferreiramanoel@gmail.com>
2026-03-10 10:38:17 -0300

    fix: try fix requests on vercel

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ searchUserForm.addEventListener("submit", async (event) => {
 
 
 async function getUserData(username){
-    const response = await fetch(`https://api.github.com/users/${username}`);
+    const response = await fetch(`https://api.github.com/users/${username}`, {
+        headers: {
+            Authorization: `token ${process.env.GITHUB_TOKEN}`
+        }
+    });
     
     if(response.ok){
         const data = await response.json();
@@ -34,14 +38,22 @@ async function getUserData(username){
 }
 
 async function getUserRepos(username){
-    const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=4`);
+    const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=4`, {
+        headers: {
+            Authorization: `token ${process.env.GITHUB_TOKEN}`
+        }
+    });
     
     const data = await response.json()
     return data;
 }
 
 async function getRepoLanguages(repo){
-    const response = await fetch(`https://api.github.com/repos/${repo}/languages`);
+    const response = await fetch(`https://api.github.com/repos/${repo}/languages`, {
+        headers: {
+            Authorization: `token ${process.env.GITHUB_TOKEN}`
+        }
+    });
 
     const data = await response.json()
     return data;
